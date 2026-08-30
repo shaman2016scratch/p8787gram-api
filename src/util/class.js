c98c61b12d2782782df45c47daa7073ba520085f
shaman2016scratch <g.ilkh@yandex.ru>
2026-08-30 16:00:43 +0300

    Update

--- src/util/class.js
+++ src/util/class.js
@@ -26,8 +26,146 @@ class User {
             session: this.session
         }
     }
+
+    joinChat (chat) {
+        this.chats.push({
+            id: chat.id,
+            config: {}
+        })
+    }
+}
+
+class Chat {
+    constructor (json) {
+        this.name = json.name || ""
+        this.id = json.id || null
+        this.members = json.members || []
+        this.type = json.type || "group"
+        this.access = json.access || "private"
+        this.admins = json.admins || []
+        this.creator = json.creator || null
+        this.owner = json.owner || this.creator
+        this.messages = json.messages || []
+        this.lastActive = json.lastActive || new Date()
+        this.createdAt = json.createdAt || new Date()
+        this.actions = json.actions || []
+    }
+
+    JSON () {
+        return {
+            id: this.id,
+            name: this.name,
+            type: this.type,
+            access: this.access,
+            members: this.members,
+            admins: this.admins,
+            owner: this.owner,
+            creator: this.creator,
+            messages: this.messages,
+            lastActive: this.lastActive,
+            createdAt: this.createdAt,
+            actions: this.actions
+        }
+    }
+
+    joinUser (user) {
+        this.members.push({
+            id: user.id,
+            joinedAt: new Date()
+        })
+        this.messages.push({
+            author: {
+                id: user.id,
+                username: user.username
+            },
+            type: "joined",
+            added: {
+                id: user.id,
+                username: user.username
+            },
+            deleted: false,
+            date: new Date()
+        })
+        this.actions.push({
+            author: {
+                id: user.id,
+                username: user.username
+            },
+            type: "joined",
+            added: {
+                id: user.id,
+                username: user.username
+            },
+            date: new Date()
+        })
+    }
+
+    addUser (user, adder) {
+        this.members.push({
+            id: user.id,
+            joinedAt: new Date()
+        })
+        this.messages.push({
+            author: {
+                id: adder.id,
+                username: adder.username
+            },
+            type: "joined",
+            added: {
+                id: user.id,
+                username: user.username
+            },
+            deleted: false,
+            date: new Date()
+        })
+        this.actions.push({
+            author: {
+                id: adder.id,
+                username: adder.username
+            },
+            type: "joined",
+            added: {
+                id: user.id,
+                username: user.username
+            },
+            date: new Date()
+        })
+    }
+
+    addAdmin (user, adder) {
+        this.admins.push({
+            id: user.id,
+            addedAt: new Date()
+        })
+        this.messages.push({
+            author: {
+                id: adder.id,
+                username: adder.username
+            },
+            type: "newAdmin",
+            added: {
+                id: user.id,
+                username: user.username
+            },
+            deleted: false,
+            date: new Date()
+        })
+        this.actions.push({
+            author: {
+                id: adder.id,
+                username: adder.username
+            },
+            type: "newAdmin",
+            added: {
+                id: user.id,
+                username: user.username
+            },
+            date: new Date()
+        })
+    }
 }
 
 export {
-    User
+    User,
+    Chat
 }
