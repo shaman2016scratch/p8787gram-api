fd3eb8007573c54e040a2a9c2831e2636a98037d
shaman2016scratch <g.ilkh@yandex.ru>
2026-08-31 09:34:25 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 import express from "express";
+import cors from "cors"
 
 const app = express();
+
+app.use(
+	express.json({ limit: "5mb" }),
+	cors(),
+	cookieParser()
+)
 
 export {
 	app as default
--- src/db.js
+++ src/db.js
@@ -1,13 +1,6 @@
 import Server8787DB from "./util/db.js";
 import config from "../config.js";
-import cors from "cors"
 
 const DB = new Server8787DB("polzovatel_8787", "p8787gram", process.env.DB_SECRET)
-
-app.use(
-	express.json({ limit: "5mb" }),
-	cors(),
-	cookieParser()
-)
 
 export default DB
